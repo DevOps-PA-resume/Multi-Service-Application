d799b9dadb2e656c06b9b19bade438603777870a
TheRealPad <pierre-alexandre.delgado-arevalo@epitech.eu>
2025-12-05 23:31:54 +0100

    add log configuration and fix vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-export default ({ mode }) => {
+export default ({ mode }: { mode: string }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
 
   return defineConfig({
